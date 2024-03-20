dd0f3d16123cc34f5ec59bf6ae0faeab48ea527f
Huseyin Eksici <huseyineksici01@gmail.com>
2024-03-21 00:51:02 +0300

    creating database and tables

--- WebBlog.DataAccesLayer/Data/ApplicationDbContext.cs
+++ WebBlog.DataAccesLayer/Data/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBlog.Entitiy.Entities;
 
 namespace WebBlog.DataAccesLayer.Data
 {
@@ -11,10 +12,37 @@ namespace WebBlog.DataAccesLayer.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        public DbSet<AppUser> AppUsers { get; set; }
 
+        public DbSet<Article> Articles { get; set; }
+
+        public DbSet<Comment>  Comments { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           
+            modelBuilder.Entity<AppUser>(entity =>
+            {
+                entity.Property(i => i.Id).HasColumnName("id").UseIdentityColumn();
+                entity.Property(i => i.email).HasColumnName("email");
+                entity.Property(i => i.firstName).HasColumnName("first_name");
+                entity.Property(i => i.lastName).HasColumnName("last_name");
+            });
+            modelBuilder.Entity<Article>(entity =>
+            {
+                entity.Property(i => i.Id).UseIdentityColumn().HasColumnName("id");
+                entity.Property(i => i.Title).HasColumnName("title");
+                entity.Property(i => i.Description).HasColumnName("description");
+                entity.HasOne(i => i.User).WithMany(i => i.Articles).HasForeignKey(i => i.userId).OnDelete(DeleteBehavior.Restrict);
+                
+            });
+            modelBuilder.Entity<Comment>(entity =>
+            {
+
+                entity.Property(i => i.Id).HasColumnName("id").UseIdentityColumn();
+                entity.Property(i => i.commmentDescription).HasColumnName("comment");
+                entity.HasOne(i => i.article).WithMany(i => i.Comments).HasForeignKey(i => i.articleId).OnDelete(DeleteBehavior.Restrict);
+                entity.HasOne(i => i.User).WithMany(i => i.Comments).HasForeignKey(i => i.userId).OnDelete(DeleteBehavior.Restrict);
+
+            });
         }
     }
 }
---
+++ WebBlog.DataAccesLayer/Migrations/20240320214320_sa.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebBlog.DataAccesLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class sa : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppUsers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUsers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    userId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Articles_AppUsers_userId",
+                        column: x => x.userId,
+                        principalTable: "AppUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    updatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    userId = table.Column<int>(type: "int", nullable: false),
+                    articleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Comments_AppUsers_userId",
+                        column: x => x.userId,
+                        principalTable: "AppUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_Articles_articleId",
+                        column: x => x.articleId,
+                        principalTable: "Articles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_userId",
+                table: "Articles",
+                column: "userId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_articleId",
+                table: "Comments",
+                column: "articleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_userId",
+                table: "Comments",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "AppUsers");
+        }
+    }
+}
